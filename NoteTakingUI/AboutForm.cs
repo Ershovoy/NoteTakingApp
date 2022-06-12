01543443853035c9022463aa6146e569781e8398
Ershovoy <ershov64372@gmail.com>
2022-06-12 08:29:35 +0300

    feature(AboutForm): Added GitHub label link functionality
    Clicking on the GitHub label link now redirect user to repository of this project

--- NoteTakingUI/AboutForm.cs
+++ NoteTakingUI/AboutForm.cs
@@ -6,4 +6,22 @@ public partial class AboutForm : Form
 	{
 		InitializeComponent();
 	}
+
+	private void GitHubLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+	{
+		System.Diagnostics.ProcessStartInfo githubURL = new()
+		{
+			FileName = "https://github.com/Ershovoy",
+			UseShellExecute = true
+		};
+
+		try
+		{
+			System.Diagnostics.Process.Start(githubURL);
+		}
+		catch (Exception exception)
+		{
+			MessageBox.Show(exception.Message, "Error occured");
+		}
+	}
 }
