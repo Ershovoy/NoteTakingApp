f023e3c42154140128e2a861b3da71435f1bbd67
Ershovoy <ershov64372@gmail.com>
2022-06-12 13:48:52 +0300

    refactoring(MainForm): body of the namespace is removed

--- NoteTakingUI/MainForm.cs
+++ NoteTakingUI/MainForm.cs
@@ -1,114 +1,113 @@
 ﻿using NoteTaking;
 
-namespace NoteTakingUI
+namespace NoteTakingUI;
+
+public partial class MainForm : Form
 {
-	public partial class MainForm : Form
+	/// <summary>
+	/// Конструкторы.
+	/// </summary>
+
+	/// <summary>
+	/// Конструктор формы по умолчанию.
+	/// </summary>
+	public MainForm()
 	{
-		/// <summary>
-		/// Конструкторы.
-		/// </summary>
+		InitializeComponent();
 
-		/// <summary>
-		/// Конструктор формы по умолчанию.
-		/// </summary>
-		public MainForm()
+		_notes = new();
+	}
+
+	/// <summary>
+	/// Ивенты.
+	/// </summary>
+
+	/// <summary>
+	/// Отобразить информации о приложении.
+	/// </summary>
+	private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+	{
+		AboutForm aboutForm = new();
+		aboutForm.ShowDialog();
+	}
+
+	/// <summary>
+	/// Создать новую заметку.
+	/// </summary>
+	private void CreateNoteButton_Click(object sender, EventArgs e)
+	{
+		Note newNote = new();
+		NoteEditForm noteEditForm = new(newNote);
+		DialogResult result = noteEditForm.ShowDialog();
+		if (result == DialogResult.OK)
 		{
-			InitializeComponent();
-
-			_notes = new();
+			_notes.AddNote(newNote);
+			NotesListBox.Items.Add(newNote.Title);
+			NotesListBox.SelectedIndex = _notes.NotesCount - 1;
 		}
+	}
 
-		/// <summary>
-		/// Ивенты.
-		/// </summary>
-
-		/// <summary>
-		/// Отобразить информации о приложении.
-		/// </summary>
-		private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+	/// <summary>
+	/// Редактировать выбранную заметку.
+	/// </summary>
+	private void ChangeNoteButton_Click(object sender, EventArgs e)
+	{
+		int selectedNoteIndex = NotesListBox.SelectedIndex;
+		try
 		{
-			AboutForm aboutForm = new();
-			aboutForm.ShowDialog();
-		}
-
-		/// <summary>
-		/// Создать новую заметку.
-		/// </summary>
-		private void CreateNoteButton_Click(object sender, EventArgs e)
-		{
-			Note newNote = new();
-			NoteEditForm noteEditForm = new(newNote);
+			Note selectedNote = _notes[selectedNoteIndex];
+			NoteEditForm noteEditForm = new(selectedNote);
 			DialogResult result = noteEditForm.ShowDialog();
 			if (result == DialogResult.OK)
 			{
-				_notes.AddNote(newNote);
-				NotesListBox.Items.Add(newNote.Title);
-				NotesListBox.SelectedIndex = _notes.NotesCount - 1;
+				NotesListBox.Items[selectedNoteIndex] = selectedNote.Title;
 			}
 		}
-
-		/// <summary>
-		/// Редактировать выбранную заметку.
-		/// </summary>
-		private void ChangeNoteButton_Click(object sender, EventArgs e)
+		catch (Exception exception)
 		{
-			int selectedNoteIndex = NotesListBox.SelectedIndex;
-			try
-			{
-				Note selectedNote = _notes[selectedNoteIndex];
-				NoteEditForm noteEditForm = new(selectedNote);
-				DialogResult result = noteEditForm.ShowDialog();
-				if (result == DialogResult.OK)
-				{
-					NotesListBox.Items[selectedNoteIndex] = selectedNote.Title;
-				}
-			}
-			catch (Exception exception)
-			{
-				MessageBox.Show(exception.Message, "Error occured");
-			}
+			MessageBox.Show(exception.Message, "Error occured");
 		}
-
-		/// <summary>
-		/// Удалить выбранную заметку.
-		/// </summary>
-		private void RemoveNoteButton_Click(object sender, EventArgs e)
-		{
-			int selectedNoteIndex = NotesListBox.SelectedIndex;
-			try
-			{
-				_notes.RemoveNote(selectedNoteIndex);
-				NotesListBox.Items.RemoveAt(selectedNoteIndex);
-			}
-			catch (Exception exception)
-			{
-				MessageBox.Show(exception.Message, "Error occured");
-			}
-		}
-
-		/// <summary>
-		/// Обновить содержимое выбранной заметки на экране.
-		/// </summary>
-		private void NotesListBox_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			if (NotesListBox.SelectedIndex != -1)
-			{
-				Note selectedNote = _notes[NotesListBox.SelectedIndex];
-				NoteTitleLabel.Text = selectedNote.Title;
-				NoteCategoryLabel.Text = string.Format("Category: {0}", selectedNote.Category);
-				NoteTextRichTextBox.Text = selectedNote.Text;
-				NoteCreateDateTime.Value = selectedNote.CreatingTime;
-				NoteModifiedDateTime.Value = selectedNote.ModifiedTime;
-			}
-		}
-
-		/// <summary>
-		/// Реализация.
-		/// </summary>
-
-		/// <summary>
-		/// Список заметок.
-		/// </summary>
-		private Notebook _notes;
 	}
+
+	/// <summary>
+	/// Удалить выбранную заметку.
+	/// </summary>
+	private void RemoveNoteButton_Click(object sender, EventArgs e)
+	{
+		int selectedNoteIndex = NotesListBox.SelectedIndex;
+		try
+		{
+			_notes.RemoveNote(selectedNoteIndex);
+			NotesListBox.Items.RemoveAt(selectedNoteIndex);
+		}
+		catch (Exception exception)
+		{
+			MessageBox.Show(exception.Message, "Error occured");
+		}
+	}
+
+	/// <summary>
+	/// Обновить содержимое выбранной заметки на экране.
+	/// </summary>
+	private void NotesListBox_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		if (NotesListBox.SelectedIndex != -1)
+		{
+			Note selectedNote = _notes[NotesListBox.SelectedIndex];
+			NoteTitleLabel.Text = selectedNote.Title;
+			NoteCategoryLabel.Text = string.Format("Category: {0}", selectedNote.Category);
+			NoteTextRichTextBox.Text = selectedNote.Text;
+			NoteCreateDateTime.Value = selectedNote.CreatingTime;
+			NoteModifiedDateTime.Value = selectedNote.ModifiedTime;
+		}
+	}
+
+	/// <summary>
+	/// Реализация.
+	/// </summary>
+
+	/// <summary>
+	/// Список заметок.
+	/// </summary>
+	private Notebook _notes;
 }
